9a9a88e9b04c727a85d02d1b297c0c51353ae11e
sj.hong <Hong1802!@>
2018-10-24 18:26:44 +0900

    ch7 commit

---
+++ hsj/ch7/cpp7_2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Rect;
+
+class RectManager {
+    public : 
+        bool equals(Rect r, Rect s);
+};
+
+class Rect {
+    int width, height;
+public : 
+    Rect(int width, int height){
+        this->width = width;
+        this->height = height;
+    }
+    friend bool RectManager::equals(Rect r, Rect s);
+};
+
+bool RectManager::equals(Rect r, Rect s){
+    if(r.width == s.width && r.height == s.height) return true;
+    else return false;
+}
+
+int main(){
+    Rect a(3,4);
+    Rect b(3,4);
+
+    RectManager man;
+    if(man.equals(a, b)) cout << "equals" << endl;
+    else cout << "not equals" << endl;
+    
+    
+}
